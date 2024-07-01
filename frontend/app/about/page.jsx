98ee49e7152c10643db566175520b81f9e415566
sambhav874 <sambhavjain874@gmail.com>
2024-07-01 23:41:48 +0530

    try

--- frontend/app/about/page.jsx
+++ frontend/app/about/page.jsx
@@ -1,6 +1,10 @@
 const about = () => {
     return(
-        'Hi'
+        <div className="flex  justify-center items-center text-center bg-pink-600">
+            <h1 className="text-[400px] font-bold tracking-wider font-serif">
+                'Grow more Loans'
+            </h1>
+        </div>
     )
 }
 
